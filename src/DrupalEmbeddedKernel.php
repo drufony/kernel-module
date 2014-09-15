e9a18db0a9d42feb68dcda99b885beffdeb3533f
Benjamin Doherty <bjd@bangpound.org>
2014-09-14 23:42:55 -0500

    put drupal in symfony kernel version.

--- src/DrupalEmbeddedKernel.php
+++ src/DrupalEmbeddedKernel.php
@@ -7,6 +7,9 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
  */
 abstract class DrupalEmbeddedKernel extends BaseKernel
 {
+
+    const EXTRA_VERSION   = 'drupal';
+
     /**
      * {@inheritdoc}
      *
