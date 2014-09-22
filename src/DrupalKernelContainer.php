5843d351f7997932c81e7f06cac71a8f036ebb81
Benjamin Doherty <bjd@bangpound.org>
2014-09-22 04:28:08 -0500

    switch to pimple kernel container.

---
+++ src/DrupalKernelContainer.php
@@ -0,0 +1,34 @@
+<?php
+
+class DrupalKernelContainer
+{
+  private static $containers = array();
+
+  public static function get($name = 'app')
+  {
+    if (!isset(self::$containers[$name])) {
+      composer_manager_register_autoloader();
+      $c = new \Pimple\Container();
+
+      $hook = 'service_provider';
+      $providers = array();
+
+      foreach (module_implements($hook) as $module) {
+        $function = $module . '_' . $hook;
+        if (function_exists($function)) {
+          $result = call_user_func_array($function, array($name));
+          if (isset($result)) {
+            $providers += (array) $result;
+          }
+        }
+      }
+
+      foreach ($providers as $args) {
+        call_user_func_array(array($c, 'register'), (array) $args);
+      }
+      self::$containers[$name] = $c;
+    }
+
+    return self::$containers[$name];
+  }
+}
---
+++ src/DrushKernelServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+
+class DrushKernelServiceProvider implements \Pimple\ServiceProviderInterface
+{
+
+  /**
+   * Registers services on the given container.
+   *
+   * This method should only be used to configure services and parameters.
+   * It should not get services.
+   *
+   * @param \Pimple\Container $pimple An Container instance
+   */
+  public function register(\Pimple\Container $pimple)
+  {
+    $pimple['console.application'] = function ($c) {
+      $application = new Application($c['kernel']);
+      $application->setAutoExit(false);
+
+      return $application;
+    };
+  }
+}
